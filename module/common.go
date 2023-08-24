c61bc73b94f453d506fbfccfe18b3a82270cccea
similar <similar2022@163.com>
2023-08-24 09:14:15 +0800

    update

--- module/common.go
+++ module/common.go
@@ -57,34 +57,19 @@ type Like struct {
 }
 
 type User struct {
-	// gorm.Model      `json:"-"`
-	// UserId          int64  `json:"id,omitempty" gorm:"index:idx_user_id"`                           // 用户id
-	// Name            string `json:"name" binding:"required" gorm:"unique;not null;index:idx_name"` 	// 用户名称
-	// Password        string `json:"-" binding:"required" gorm:"not null"`
-	// FollowCount     int64  `json:"follow_count" gorm:"default:0"` 	// 关注总数
-	// FollowerCount   int64  `json:"follower_count"`                  // 粉丝总数
-	// IsFollow        bool   `json:"is_follow"`                  		// true-已关注，false-未关注
-	// Avatar          string `json:"avatar"`                  		// 用户头像
-	// BackgroundImage string `json:"background_image"`                // 用户个人页顶部大图
-	// Signature       string `json:"signature"`                  		// 个人简介
-	// TotalFavorited  string `json:"total_favorited"`                 // 获赞数量
-	// WorkCount       int    `json:"work_count"`                  	// 作品数量
-	// FavoriteCount   int    `json:"favorite_count"`                  // 点赞数量
-	// Token           string `json:"-" gorm:"-"`
-
 	gorm.Model      `json:"-"`
 	UserId          int64  `json:"id,omitempty" gorm:"index:idx_user_id"`                           // 用户id
 	Name            string `json:"name" binding:"required" gorm:"unique;not null;index:idx_name"` 	// 用户名称
 	Password        string `json:"-" binding:"required" gorm:"not null"`
-	FollowCount     int64  `json:"follow_count" gorm:"default:0"` 	// 关注总数
-	FollowerCount   int64  `json:"follower_count" gorm:"default:0"`                  // 粉丝总数
-	IsFollow        bool   `json:"is_follow" gorm:"default:true"`                  		// true-已关注，false-未关注
-	Avatar          string `json:"avatar" gorm:"default:null"`                  		// 用户头像
-	BackgroundImage string `json:"background_image"`                // 用户个人页顶部大图
-	Signature       string `json:"signature"`                  		// 个人简介
-	TotalFavorited  int    `json:"total_favorited" gorm:"default:0"`                 // 获赞数量
-	WorkCount       int    `json:"work_count"`                  	// 作品数量
-	FavoriteCount   int64    `json:"favorite_count"`                  // 点赞数量
+	FollowCount     int64  `json:"follow_count" gorm:"default:0"` 									// 关注总数
+	FollowerCount   int64  `json:"follower_count" gorm:"default:0"`                  				// 粉丝总数
+	IsFollow        bool   `json:"is_follow" gorm:"default:true"`                  					// true-已关注，false-未关注
+	Avatar          string `json:"avatar" gorm:"default:null"`                  					// 用户头像
+	BackgroundImage string `json:"background_image"`                								// 用户个人页顶部大图
+	Signature       string `json:"signature"`                  										// 个人简介
+	TotalFavorited  int    `json:"total_favorited" gorm:"default:0"`                 				// 获赞数量
+	WorkCount       int    `json:"work_count"`                  									// 作品数量
+	FavoriteCount   int64    `json:"favorite_count"`                  								// 点赞数量
 	Token           string `json:"-" gorm:"-"`
 }
 
